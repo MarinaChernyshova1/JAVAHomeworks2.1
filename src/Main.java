96a6b3b60a2ad60c715da1412283d0e11794ae06
MarinaCh <marina3123273@mail.ru>
2023-04-09 17:13:09 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        int initialAccount = 35;
-        int depositAmount = 1000;
-        int y = 100;
-        int x;
+        int initialAccount = 46;
+        int depositAmount = 1500;
+        int replenishmentVariable = 100;
+        int grandTotal;
 
         if (depositAmount > 1000) {
-            x = (depositAmount / y) + depositAmount + initialAccount;
-            System.out.println("Итоговая сумма на счету клиента " + x);
+            grandTotal = (depositAmount / replenishmentVariable) + depositAmount + initialAccount;
+            System.out.println("Итоговая сумма на счету клиента " + grandTotal);
             System.out.println("Итоговая сумма бонусов " + (depositAmount / 100));
 
         } else {
-            x = depositAmount + 0 + initialAccount;
-            System.out.println("Итоговая сумма на счету клиента " + x);
+            grandTotal = depositAmount + 0 + initialAccount;
+            System.out.println("Итоговая сумма на счету клиента " + grandTotal);
             System.out.println("Итоговая сумма бонусов " + 0);
         }
     }
